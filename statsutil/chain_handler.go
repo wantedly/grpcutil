262585e797aea1af964b3a7b1e0e0e100f30f85f
izumin5210 <m@izum.in>
2018-08-08 09:09:20 +0900

    Change grpcutil.ChainStatsHandler -> statsutil.ChainHandler

--- statsutil/chain_handler.go
+++ statsutil/chain_handler.go
@@ -1,4 +1,4 @@
-package grpcutil
+package statsutil
 
 import (
 	"context"
@@ -6,44 +6,44 @@ import (
 	"google.golang.org/grpc/stats"
 )
 
-type chainedStatsHandler struct {
+type chainedHandler struct {
 	handlers []stats.Handler
 }
 
-// ChainStatsHandlers creates a single stats handler out of a chain of many stats handlers.
+// ChainHandlers creates a single stats handler out of a chain of many stats handlers.
 //
 // Execution is done in left-to-right order, including passing of context.
 // For example ChainUnaryServer(one, two, three) will execute one before two before three, and three
 // will see context changes of one and two.
 //
 // See https://godoc.org/github.com/grpc-ecosystem/go-grpc-middleware#ChainUnaryServer
-func ChainStatsHandlers(handlers ...stats.Handler) stats.Handler {
-	return &chainedStatsHandler{
+func ChainHandlers(handlers ...stats.Handler) stats.Handler {
+	return &chainedHandler{
 		handlers: handlers,
 	}
 }
 
-func (csh *chainedStatsHandler) TagRPC(ctx context.Context, info *stats.RPCTagInfo) context.Context {
+func (csh *chainedHandler) TagRPC(ctx context.Context, info *stats.RPCTagInfo) context.Context {
 	for _, h := range csh.handlers {
 		ctx = h.TagRPC(ctx, info)
 	}
 	return ctx
 }
 
-func (csh *chainedStatsHandler) HandleRPC(ctx context.Context, s stats.RPCStats) {
+func (csh *chainedHandler) HandleRPC(ctx context.Context, s stats.RPCStats) {
 	for _, h := range csh.handlers {
 		h.HandleRPC(ctx, s)
 	}
 }
 
-func (csh *chainedStatsHandler) TagConn(ctx context.Context, info *stats.ConnTagInfo) context.Context {
+func (csh *chainedHandler) TagConn(ctx context.Context, info *stats.ConnTagInfo) context.Context {
 	for _, h := range csh.handlers {
 		ctx = h.TagConn(ctx, info)
 	}
 	return ctx
 }
 
-func (csh *chainedStatsHandler) HandleConn(ctx context.Context, s stats.ConnStats) {
+func (csh *chainedHandler) HandleConn(ctx context.Context, s stats.ConnStats) {
 	for _, h := range csh.handlers {
 		h.HandleConn(ctx, s)
 	}
